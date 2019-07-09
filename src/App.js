929b9b96bfcbd8c45d855b350eff8df4ca5430f8
garyos <g.osman89@gmail.com>
2019-07-09 19:24:10 +0100

    react-workshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import spiderman from './spiderman.jpg'
+import toystory from './toystory4.jpg'
+import megashark from './megashark.jpg'
+
+function Movie(props) {
+  return (<div>
+      <h2>{props.title} ({props.year})</h2>
+      <img src={props.image}/>
+    </div>);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the app</h1>
+       <div>
+        <Movie title='Spiderman' image={spiderman} year='2001'/>
+        <Movie title='Toy Story' image={toystory} year='2002'/>
+        <Movie title='Megashark' image={megashark} year='2003'/>
+      </div>
     </div>
+
   );
 }
 
